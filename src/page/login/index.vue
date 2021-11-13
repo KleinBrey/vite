616d8369f33a0ea8cd29c04eee540a9502971c29
kleinbrey <55785073+duonanjian@users.noreply.github.com>
2021-11-13 19:07:31 +0800

    33
    333

---
+++ src/page/login/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <div class="logincontent"></div>
+  </div>
+</template>
+<script setup>
+</script>
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background-image: url("/@/assets/img/homepage.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  .logincontent {
+    width: 300px;
+    height: 500px;
+    background-color: aqua;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
