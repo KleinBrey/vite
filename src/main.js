38ee902d134ca435c5684a6d4a358a0a2b7d17ae
kleinbrey <55785073+duonanjian@users.noreply.github.com>
2021-12-23 21:00:58 +0800

    55
    55

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ import store from './store';
 import  useElementPlus from "../src/plugins/element-plus/index";
 import  useTable from "../src/plugins/vxe-table/index";
 import 'element-plus/dist/index.css'
-
+import './permission'
 import './assets/css/index.css'
-console.log(import.meta.env)
-console.log(ElementPlus)
+
 const app = createApp(App)
-app.use(ElementPlus).use(useTable).use(store).use(router)
+app.use(useElementPlus).use(useTable).use(store).use(router)
 app.mount('#app')
